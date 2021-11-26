d9dc75f779444a81b6b255dbd5ca8e22c01bccc0
Dimentorr <stepapetruk@ya.ru>
2021-11-26 19:42:14 +1000

    yeallow circle

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            self.do_paint = False
+            qp.end()
+
+    def draw_flag(self, qp):
+        # qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        qp.setBrush(QColor(255, 255, 0))
+        tmp = random.randint(30, 150)
+        qp.drawEllipse(50, 30, tmp, tmp)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
